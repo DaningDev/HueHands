2bb920e0d4d6110b1c01d7c26743a22130d746d8
Simon Szydlik <daningteen@gmail.com>
2019-02-27 14:01:43 +0100

    [Added] Base project
    The beginning of something great

---
+++ hue_hands/hue_hands/main.c
@@ -0,0 +1,18 @@
+/*
+ * hue_hands.c
+ *
+ * Created: 2/27/2019 2:00:31 PM
+ * Author : danin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
