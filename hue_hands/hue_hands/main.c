838436ffb57f5439b2f4872d630e21e7b924f680
Simon Szydlik <daningteen@gmail.com>
2019-03-20 11:40:30 +0100

    [Added] Button

--- hue_hands/hue_hands/main.c
+++ hue_hands/hue_hands/main.c
@@ -9,61 +9,84 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define LED_ON		PORTD &= ~(1 << PORTD3)
-#define LED_OFF		PORTD |= (1 << PORTD3)
+#define LED_ON		PORTD |= (1 << PORTD3)
+#define LED_OFF		PORTD &= ~(1 << PORTD3)
 
 #define TRIGGER_ON	PORTD |= (1 << PORTD2)
 #define TRIGGER_OFF	PORTD &= ~(1 << PORTD2)
 
 double calc_dist(void);
+int returnButton(void);
 void wait (double ms);
+void initTimerPWM(void);
 
 int main(void)
 {
 	DDRD |= (1 << PORTD3); 
 	DDRD |= (1 << PORTD2);
 	
+	//Does it live?
+ 	LED_ON;
+	wait(1000);
 	LED_OFF;
+	
+	//Just to be safe.
 	TRIGGER_OFF;
 	
-	double distance = 0;
-	distance = calc_dist();
-	
-	//check if there is difference in distance by having varying how long the LED is turned on.
-	LED_ON;
-	wait(distance*100);
-	LED_OFF;
-	
     while (1) 
-    {			
+    {
+		if(!(PINB & (1 << PINB0)))
+		{
+			double distance = 0;
+			distance = calc_dist();
+			
+			LED_ON;
+			wait(distance*100);
+			LED_OFF;
+		} 
+		else
+		LED_OFF;
     }
 }
 
 double calc_dist(void)
 {
-	//test.
-	TCNT1H = 0;
-	TCNT1L = 0;
+	//Reset last timer.
+	TCCR1B &= (~(1 << CS10) | ~(1 << CS12));
+	TCNT1 = 0;
 	
 	//send trigger to ultra sonic sensor.
 	TRIGGER_ON;
 	_delay_us(15);
 	TRIGGER_OFF;
 	
-	//set clock no pre-scaler when echo signal is high (1).
+	//Wait until echo signal is high (1).
 	while(!(PIND & (1 << PIND4)));
 	
+	//Start Timer with prescaler of 1024.
 	TCCR1B |= ((1 << CS10) | (1 << CS12));
 	
-	//stop when echo signal is low (0).
+	//Wait until echo signal is low (0).
 	while((PIND & (1 << PIND4)));
-	
 	
 	//read the clock value and do calculations. TODO
 	return TCNT1;
 }
 
-void wait (double ms) {
+void initTimerPWM()
+{
+	TCCR0A = 
+	TCCR0B = 
+	OCR1A = 0;
+}
+
+void setPulse(unsigned char red)
+{
+	OCR1A = red;
+}
+
+void wait (double ms) 
+{
 	for (int i = 0; i<ms; i++){
 		_delay_ms(1);
 	}
