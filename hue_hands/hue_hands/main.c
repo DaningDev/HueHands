9bd1a732e3b28aab197207bc2b1af15648074e83
Simon Szydlik <daningteen@gmail.com>
2019-03-20 13:30:01 +0100

    [Added] beginning of PWM

--- hue_hands/hue_hands/main.c
+++ hue_hands/hue_hands/main.c
@@ -19,11 +19,14 @@ double calc_dist(void);
 int returnButton(void);
 void wait (double ms);
 void initTimerPWM(void);
+void setPulse(unsigned char red);
+void setPWM(void);
 
 int main(void)
 {
 	DDRD |= (1 << PORTD3); 
 	DDRD |= (1 << PORTD2);
+	DDRB |= (1 << PORTB6);
 	
 	//Does it live?
  	LED_ON;
@@ -75,14 +78,37 @@ double calc_dist(void)
 
 void initTimerPWM()
 {
-	TCCR0A = 
-	TCCR0B = 
-	OCR1A = 0;
+	TCCR0A |= ((1 << FOC0A) | (1 << COM0A0) | (1 << CS00)); //Set on match, clear on TOP at 0C0A (=pd6)
+	TCCR0B |= ((1 << WGM00) | (1 << CS00)); //Start timer with PWM mode
+
+	OCR0A = 128; 
 }
 
 void setPulse(unsigned char red)
 {
-	OCR1A = red;
+	OCR0A = red;
+}
+
+void setPWM()
+{
+	int delta = 1;
+	setPulse(0);
+	
+	for(int red = 0; red<=255; red+=delta)
+	{
+		setPulse(red);
+		delta+=2;
+		wait(100);
+	}
+	for(int red = 255l; red>=0; red-=delta)
+	{
+		setPulse(red);
+		delta-=2;
+		wait(100);
+	}
+	setPulse(0);
+	delta = 1;
+	wait(100);
 }
 
 void wait (double ms) 
