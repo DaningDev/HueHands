370508aa2e76d09d5f46f7892a7914c8f58e5af6
Whisper <gewoonjvr@gmail.com>
2019-03-29 13:57:39 +0100

    [Fixed] color algorithm

--- hue.py
+++ hue.py
@@ -3,22 +3,34 @@ import json
 import requests
 
 
+# functions def
+
+def getColor(cm) :
+    return cm * 1310
+
+
+# to find api on your network go to https://www.meethue.com/api/nupnp
+
 body = {"devicetype":"jordo"}
 res = requests.post("http://192.168.2.15/api", data=json.dumps(body))
-print(r.content)
-content = json.loads(r.content)
-print(content["succes"]["username"])
+print(res.content)
+content = json.loads(res.content)
 
+username = content[0]["success"]["username"]
+
+body = {"hue": getColor(10) }
+res = requests.put("http://192.168.2.15/api/" + username + "/lights/1/state",data=json.dumps(body))
+print(res.content)
 
 # init serial ports for connection with arduino
-ser = serial.Serial()
-ser.port = "COM3"
-ser.baudrate = 9600
-ser.open()
+# ser = serial.Serial()
+# ser.port = "COM3"
+# ser.baudrate = 9600
+# ser.open()
 
 
-# opens port of serial connection.
-print("reading....")
-while 1:
-    print(ser.readline(8))
+# # opens port of serial connection.
+# print("reading....")
+# while 1:
+#     print(ser.readline(8))
     
